e1365bd82a4aa71e69766ac9e58f6b2123f08765
Jamal <rajajamalparvez@gmail.com>
2026-01-13 11:52:36 +0500

    Fix SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App.tsx";
 import "./index.css";
 
